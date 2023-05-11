653b06bdda847506c4cc4264575a678f0086d0fd
martinGlvn <lkn.galvan.martin@gmail.com>
2023-05-11 03:10:34 -0300

    intermediate sintax python

---
+++ labsForge/015.py
@@ -0,0 +1,34 @@
+# Puede utilizar Linux para realizar muchas tareas administrativas desde el terminal o desde la línea de comandos de Bash. Python proporciona varios módulos que también puede utilizar para ejecutar comandos en la línea de comandos. En este laboratorio, utilizará os.system() y subprocess.run() para ejecutar comandos de Bash desde Python.
+
+# En este laboratorio, deberá realizar lo siguiente:
+
+# utilizar os.system() para ejecutar un comando de Bash
+# utilizar subprocess.run() para ejecutar comandos de Bash
+import os
+import subprocess
+
+
+subprocess.run(stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None,
+               timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None)
+
+# Uso de subprocess.run con un argumento
+os.system("ls")
+# Uso de subprocess.run con dos argumentos
+subprocess.run(["ls", "-l"])
+
+# Uso de subprocess.run con tres argumentos
+subprocess.run(["ls", "-l", "README.md"])
+
+# Recuperación de información del sistema
+command = "uname"
+commandArgument = "-a"
+print(
+    f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command, commandArgument])
+
+# Recuperación de información sobre el espacio en disco
+command = "ps"
+commandArgument = "-x"
+print(
+    f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command, commandArgument])
---
+++ labsForge/016.py
@@ -0,0 +1,11 @@
+# Un error de software se refiere a un defecto o a una falla en un programa informático que provoca un resultado incorrecto o inesperado. Un depurador es un programa informático que se utiliza para probar y encontrar errores o depurar programas. Puede utilizar un depurador para examinar el código. El depurador de Python (pdb) es un depurador de código fuente interactivo para los programas de Python. En este laboratorio, utilizará el pdb para examinar los scripts que escribió en los laboratorios anteriores.
+
+# En este laboratorio, deberá realizar lo siguiente:
+
+# explorar las características básicas del depurador de Python
+# utilizar el depurador de Python para examinar scripts
+
+name = "John"
+print("Hello " + name + ".")
+age = 40
+print(name + " is " + str(age) + " years old.")
