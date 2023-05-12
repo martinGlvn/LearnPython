27173d41b572d051415a76f63d1a57b463419df0
martinGlvn <lkn.galvan.martin@gmail.com>
2023-05-12 13:52:10 -0300

    python intermediate

--- 02SintaxIntr/09Maps.py
+++ 02SintaxIntr/09Maps.py
@@ -20,3 +20,36 @@ print(resultado)  # [6, 8, 10, 12]
 
 
 # Maps con diccionarios ---------------------------------------------
+items = [
+    {
+        'producto': 'pantalon',
+        'price': 500,
+
+    },
+    {
+        'producto': 'camisa',
+        'price': 300,
+    },
+    {
+        'producto': 'remera',
+        'price': 100,
+    }
+]
+# Solo queremos una lista con los precios unicamente
+precios = list(map(lambda item: item['price'], items))
+print(precios)  # [500, 300, 100]
+
+# Agregamos un elemento nuevo al dict con map
+
+
+def agregarImpuestos(item):
+    nuevo_item = item.copy()
+    nuevo_item['taxes'] = nuevo_item['price'] * .19
+    return nuevo_item
+
+
+nuevos = list(map(agregarImpuestos, items))
+print(nuevos)
+
+
+print(items)
