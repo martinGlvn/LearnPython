4588225edd2d440fe41e4b906d9ad5e56b9eeb99
martinGlvn <lkn.galvan.martin@gmail.com>
2023-05-14 21:56:38 -0300

    sintax basic

--- 1-Sintax/01SintaxBasic/01Variables.py
+++ 1-Sintax/01SintaxBasic/01Variables.py
@@ -1,19 +1,16 @@
-#Variables =>
-mi_primera_variable = "hola" #str
-mi_segunda_variable = 5 #int
-mi_tercera_varibale = False #bool
-mi_cuarta_varibale = True #bool
+# Variables => -
+mi_primera_variable = "hola"  # str
+mi_segunda_variable = 5  # int
+mi_tercera_varibale = False  # bool
+mi_cuarta_varibale = True  # bool
 
 
-#Funciones del sistema =>
-print(len(mi_primera_variable)) #4 -> longitud de la variable
+# Funciones del sistema =>
+print(len(mi_primera_variable))  # 4 -> longitud de la variable
 
-#Concatenacion de variables =>
+# Concatenacion de variables =>
 print("esta es una concatenacion:", mi_primera_variable)
 
-#Variables en una sola linea =>
+# Variables en una sola linea =>
 nombre, apellido, edad = "martin", "jerejere", 55
-print(nombre, apellido, edad) # 
-
-
-
+print(nombre, apellido, edad)
